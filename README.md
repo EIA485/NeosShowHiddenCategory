bad5964bc09b6afa70448b61de95ff90c4bacaab
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-10-31 23:50:26 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# ShowHiddenCategory
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that shows the hidden category.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [ShowHiddenCategory.dll](https://github.com/eia485/NeosShowHiddenCategory/releases/latest/download/ShowHiddenCategory.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ ShowHiddenCategory/ShowHiddenCategory.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
+namespace ShowHiddenCategory
+{
+	public class ShowHiddenCategory : NeosMod
+	{
+		public override string Name => "ShowHiddenCategory";
+		public override string Author => "eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/EIA485/NeosShowHiddenCategory/";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("net.eia485.ShowHiddenCategory");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(WorkerInitializer), "Initialize", new Type[]{ typeof(List<Type>) })]
+		class ShowHiddenCategoryPatch
+		{
+			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+			{
+				var codes = new List<CodeInstruction>(instructions);
+				for (int i = 0; i < codes.Count; i++)
+				{
+					if (codes[i].opcode.OperandType == OperandType.InlineString & codes[i].operand?.ToString() == "Hidden")
+					{
+						for (int si = i - 1; si <i+3; si++)
+						{
+							codes[si].opcode = OpCodes.Nop;
+						}
+						break;
+						
+					}
+				}
+				return codes;
+			}
+		}
+	}
+}
